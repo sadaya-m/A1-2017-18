8bc3b6297455ee2a985d56450104be30d846039c
sadaya-m <sadaya-m@1W07-10.uwds.uwinnipeg.ca>
2017-11-23 09:22:52 -0600

    Array Warning and Null Exception Pointer

--- src/A1Q2.java
+++ src/A1Q2.java
@@ -2,43 +2,41 @@
 //  Programmer:         Ma Pauline Sadaya
 //  Class:		CS30S
 //
-//  Assignment:         A1Q2
+//  Assignment:         aX qy
 //
 //  Description:	a brief description of the program goes here
+//
 //
 //  Input:		describe any input from keyboard or file
 //
 //  Output:		describe the result of your program
 // ***********************************************************************
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.swing.*;
 import java.text.DecimalFormat;
+import java.io.*;
 
 public class A1Q2/*CHANGE THIS TO THE NAME OF THE FILE*/
 {  // begin class
     public static void main(String args[]) throws IOException
     {  // begin main
     // ***** declaration of constants *****
-
+        
+        final int MAXLISTSIZE = 10;
+        
     // ***** declaration of variables *****
         
-        String strin;
         int count = 0;
-        int list[] = new int[10];
+        int list[] = new int [MAXLISTSIZE];
+        
+        String l;
         
         BufferedReader numlist = null;
 
-    // ***** create objects **********
+    // ***** create objects *****
 
 
-    // ***** create input stream ***** 
+    // ***** create input stream *****
 
         //ConsoleReader console = new ConsoleReader(System.in);
         
@@ -47,8 +45,8 @@ public class A1Q2/*CHANGE THIS TO THE NAME OF THE FILE*/
         }
         catch(FileNotFoundException e){
             JOptionPane.showMessageDialog(null,"file not found");
-        }
-    	
+}
+        
 
     // ***** Print Banner *****
 
@@ -60,22 +58,21 @@ public class A1Q2/*CHANGE THIS TO THE NAME OF THE FILE*/
 
     // ***** get input *****
 
-        strin = numlist.readLine();
+        l = numlist.readLine();
         
-        while(strin != null){
-            //System.out.println(strin);
-            list[count] = Integer.parseInt(strin);
+        while(l != null){
+            list[count] = Integer.parseInt(l);
             count++;
-            strin = numlist.readLine();
+            l = numlist.readLine();
         }
-
+        
     // ***** processing *****
 
-
+        
     // ***** output *****
 
-        System.out.println(numlist);
-
+       //System.out.println(list);
+        
     // ***** closing message *****
 
         System.out.println("end of processing");
